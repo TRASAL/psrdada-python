e99b7ebb7241c0da0c7680bd91c74518da53464c
Jisk Attema <j.attema@esciencecenter.nl>
2019-01-24 14:13:46 +0100

    Add example script

---
+++ examples/sum.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import numpy as np
+from psrdada import Reader
+
+# Create a reader instace
+reader = Reader()
+
+# Connect to a running ringbuffer with key 'dada'
+reader.connect('dada')
+
+# loop over the pages
+for page in reader:
+    # read the page as numpy array
+    data = np.asarry(page)
+    print (np.sum(data))
